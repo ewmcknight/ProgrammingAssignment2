499209c2ef7932f16aea6f8b27529083047fcb8c
ewmcknight <ericmcknight@Erics-MacBook-Pro.local>
2015-07-26 15:40:45 -0700

    Assignment submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are identical in form & function as the demo functions
+## for assignment #2, "Caching the Mean of a Vector", except they have been
+## modified to compute the inverse of a matrix
 
-## Write a short comment describing this function
+## Maintain a precomputed (cached) matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse
+  )
 }
 
 
-## Write a short comment describing this function
+## find the inverse of a matrix if no cached solution exists
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if (!is.null(m)) {
+    message("returning cached matrix...")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+  m
 }
